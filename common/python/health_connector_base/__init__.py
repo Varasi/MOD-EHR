2bd1bd46f5ba120121aae32210aaa3a1c7c8c734
Sai Teja <sai.boddana@varasi.com>
2024-12-16 22:46:17 +0530

    removed the lib files

--- common/python/health_connector_base/__init__.py
+++
@@ -1,4 +0,0 @@
-from health_connector_base.cache_manager import (  # noqa
-    RedisCacheManager as CacheManager,
-)
-from health_connector_base.secrets_manager import KMSClient as SecretsManager  # noqa
